69c06a6085f324576a07b63faa2071c4a89f47ae
jmilktea <jmilktea>
2020-02-22 23:23:03 +0800

    webflux swagger

---
+++ sample/web/src/test/java/com/jmilktea/sample/web/WebApplicationTests.java
@@ -0,0 +1,66 @@
+package com.jmilktea.sample.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.CollectionUtils;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+
+@SpringBootTest
+class WebApplicationTests {
+
+    @Test
+    public void testMonoPage() {
+        executeMonoPage().block();
+    }
+
+    public Mono<Boolean> executeMonoPage() {
+        AtomicReference<Integer> index = new AtomicReference<>(0);
+        Integer size = 10;
+        return queryData(index.get(), size).expand(list -> {
+            if (CollectionUtils.isEmpty(list)) {
+                return Mono.empty();
+            }
+            return queryData(index.getAndSet(index.get() + 1), size);
+        }).flatMap(list -> {
+            //执行完queryData后即会执行遍历，然后再执行下一次queryData
+            executeMonoPageError(list);
+            list.forEach(s -> {
+                System.out.println(s);
+            });
+            return Mono.just(true);
+        }).onErrorContinue((t,d)->{
+            System.out.println(t);
+        }).collectList().flatMap(s -> Mono.just(true));
+    }
+
+    /**
+     * 这里可以是数据库，或者调用接口等耗时操作
+     *
+     * @param index
+     * @param size
+     * @return
+     */
+    public Mono<List<Integer>> queryData(Integer index, Integer size) {
+        if (index == 10) {
+            return Mono.empty();
+        }
+        List<Integer> list = new ArrayList<>();
+        for (Integer i = index * size; i < ((index + 1) * size); i++) {
+            list.add(i);
+        }
+        return Mono.just(list);
+    }
+
+    public void executeMonoPageError(List<Integer> list) {
+        if (list.get(0).equals(10)) {
+            throw new RuntimeException();
+        }
+    }
+}
