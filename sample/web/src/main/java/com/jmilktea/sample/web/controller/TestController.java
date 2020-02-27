70da1d7f51abc238b285b701b8bfd770b4fa9278
jmilktea <jmilktea>
2020-02-27 22:45:15 +0800

    merge squash

--- sample/web/src/main/java/com/jmilktea/sample/web/controller/TestController.java
+++ sample/web/src/main/java/com/jmilktea/sample/web/controller/TestController.java
@@ -34,4 +34,8 @@ public class TestController {
     public Mono<ReactiveResult> error() {
         return ReactiveResult.fail("1001", "1001 error");
     }
+
+    //git ff 1 2
+    //git nf 1 2
+    //git squash 1 2 3 4 5
 }
