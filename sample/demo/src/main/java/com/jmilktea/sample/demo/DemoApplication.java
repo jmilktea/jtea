e913ffca0fd37771dfbd6c36e0d8d8a3c8292ba8
huangyb <huangyb@akulaku.com>
2020-07-22 17:05:54 +0800

    tag1.2

--- sample/demo/src/main/java/com/jmilktea/sample/demo/DemoApplication.java
+++ sample/demo/src/main/java/com/jmilktea/sample/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		//git tag v1.1
+		//git tag v1.2
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
