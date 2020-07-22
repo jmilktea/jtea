adec15dcd8ae96f91a2bfa2faeaf077c4d6fe7e5
huangyb <huangyb@akulaku.com>
2020-07-22 16:31:01 +0800

    tag1.0

--- sample/demo/src/main/java/com/jmilktea/sample/demo/DemoApplication.java
+++ sample/demo/src/main/java/com/jmilktea/sample/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		//git tag v1.0
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
